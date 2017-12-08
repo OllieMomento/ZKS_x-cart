e4fab2e94338bb905ace1d2e2222ea823f1e3fb2
OllieMomento <ollie.chvila@gmail.com>
2017-12-08 18:36:44 +0100

    dobre depository

--- X-Cart.com/src/main/java/mantisbt/pageObjects/LoginPageObject.java
+++ X-Cart.com/src/main/java/mantisbt/pageObjects/LoginPageObject.java
@@ -5,10 +5,15 @@
  */
 package mantisbt.pageObjects;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  *
@@ -16,14 +21,14 @@ import org.openqa.selenium.support.FindBy;
  */
 public class LoginPageObject {
 
-    private final WebDriver driver;
+    private final RemoteWebDriver driver;
     private final String baseUrl;
-    
-    @FindBy(name = "btn  regular-button  popup-button popup-login")
-    @CacheLookup
-    private WebElement login;
 
-    public LoginPageObject(String baseUrl, WebDriver driver) {
+    @FindBy(css = "#header-bar div:nth-of-type(3) button.btn.regular-button.popup-button.popup-login")
+    @CacheLookup
+    private WebElement signInsignUp;
+
+    public LoginPageObject(String baseUrl, RemoteWebDriver driver) {
 
         this.baseUrl = baseUrl;
         this.driver = driver;
@@ -32,9 +37,34 @@ public class LoginPageObject {
     public void open() {
         System.out.println("BASE URL: " + baseUrl);
         driver.get(baseUrl);
-        login.click();
-        
+        driver.manage().window().maximize();
+        signInsignUp.click();
+
     }
 
+    public void setEmailTextField(String emailValue) {
+        System.out.println("emailvalue: " + emailValue);
+        waitForElement(driver, By.name("login"));
+        driver.findElement(By.name("login")).sendKeys(emailValue);
+
+    }
+
+    public void setPasswordField(String passwordValue) {
+        driver.findElement(By.id("login-password")).sendKeys(passwordValue);
+    }
+
+    public void clickLoginButton() {
+        driver.findElement(By.cssSelector("td button.btn.regular-button.submit")).click();
+
+    }
+
+    private void waitForElement(RemoteWebDriver driver, final By by) {
+        Wait<WebDriver> wait = new WebDriverWait(driver, 10);
+        wait.until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver driver) {
+                return driver.findElement(by).isDisplayed();
+            }
+        });
+    }
 
 }
--- X-Cart.com/src/test/java/test/mantisbt/recorded/LoginPageObjectTest.java
+++ X-Cart.com/src/test/java/test/mantisbt/recorded/LoginPageObjectTest.java
@@ -5,10 +5,10 @@ package test.mantisbt.recorded;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
+import mantisbt.pageObjects.LoginPageObject;
 
 import org.apache.commons.io.FileUtils;
 import static org.junit.Assert.assertTrue;
@@ -16,20 +16,20 @@ import org.testng.annotations.*;
 import static org.testng.Assert.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestResult;
-import pageObjects.LoginPageObject;
-
-
-
 
 /**
  *
  * @author BeryUncool
  */
 public class LoginPageObjectTest {
-    
-    private WebDriver driver;
+
+    private RemoteWebDriver driver;
     private String baseUrl;
     private boolean acceptNextAlert = true;
     private final StringBuffer verificationErrors = new StringBuffer();
@@ -42,7 +42,6 @@ public class LoginPageObjectTest {
         baseUrl = "https://demostore.x-cart.com/";
         // driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         loginPage = new LoginPageObject(baseUrl, driver);
-        System.out.println("setUp");
 
     }
 
@@ -51,9 +50,13 @@ public class LoginPageObjectTest {
         PageFactory.initElements(driver, loginPage);
 
         loginPage.open();
-        String name = "chvilond";       
+        String email = "joe@doe.com";
+        String password = "joeking";
+
+        loginPage.setEmailTextField(email);
+        loginPage.setPasswordField(password);
+
+        loginPage.clickLoginButton();
 
     }
-
-    
 }
