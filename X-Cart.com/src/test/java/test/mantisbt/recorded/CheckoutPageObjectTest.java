25a4cc6f87566ae4c5815f2da127be64392305e3
OllieMomento <ollie.chvila@gmail.com>
2018-01-16 22:19:05 +0100

    zmeny

---
+++ X-Cart.com/src/test/java/test/mantisbt/recorded/CheckoutPageObjectTest.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.mantisbt.recorded;
+
+import mantisbt.pageObjects.CartPageObject;
+import mantisbt.pageObjects.HomePageObject;
+import static org.junit.Assert.assertTrue;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author Ollie
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses(
+    CartPageObjectTest.class)
+
+
+public class CheckoutPageObjectTest {
+
+
+}
